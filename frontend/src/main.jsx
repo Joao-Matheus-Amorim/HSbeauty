4f886bc7a8a755264b8f616648ea838783ac3f20
Joao-Matheus-Amorim <202209304731@alunos.estacio.br>
2026-05-01 17:53:19 -0300

    style: load site-wide glassmorphism css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './index.css'
+import './glassmorphism-site.css'
 import App from './App.jsx'
 import Admin from './pages/Admin.jsx'
 

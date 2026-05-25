0fe32c5434ff8adf4f5a0e0b761fee00a91f755b
Joao-Matheus-Amorim <joaomatheus.lab@gmail.com>
2026-05-25 15:04:58 -0300

    feat: add deprecation headers to legacy admin routes

--- backend/src/legacy-route-deprecation.js
+++ backend/src/legacy-route-deprecation.js
@@ -1,5 +1,3 @@
-import express from 'express';
-
 const LEGACY_ADMIN_ROUTES = new Set([
   'GET /agendamentos',
   'GET /agendamentos/:id',
@@ -20,44 +18,14 @@ function normalizeLegacyRoutePath(path) {
   return path;
 }
 
-function shouldMarkDeprecated(req) {
-  if (!req) return false;
-  const routeKey = `${req.method} ${normalizeLegacyRoutePath(req.path)}`;
-  return LEGACY_ADMIN_ROUTES.has(routeKey);
-}
-
-function setDeprecationHeaders(req, res) {
-  if (!shouldMarkDeprecated(req) || res.headersSent) return;
-  res.set('Deprecation', 'true');
-  res.set('Sunset', 'Wed, 30 Sep 2026 23:59:59 GMT');
-  res.set('X-HSBeauty-Deprecated-Route', 'Use the equivalent /admin route');
-}
-
 export function legacyAdminRouteDeprecation(req, res, next) {
-  setDeprecationHeaders(req, res);
+  const routeKey = `${req.method} ${normalizeLegacyRoutePath(req.path)}`;
+
+  if (LEGACY_ADMIN_ROUTES.has(routeKey)) {
+    res.set('Deprecation', 'true');
+    res.set('Sunset', 'Wed, 30 Sep 2026 23:59:59 GMT');
+    res.set('X-HSBeauty-Deprecated-Route', 'Use the equivalent /admin route');
+  }
+
   next();
 }
-
-function installLegacyRouteDeprecationPatch() {
-  if (express.response.__hsBeautyLegacyDeprecationPatchInstalled) return;
-
-  const originalJson = express.response.json;
-  const originalSend = express.response.send;
-
-  express.response.json = function jsonWithLegacyDeprecationHeaders(...args) {
-    setDeprecationHeaders(this.req, this);
-    return originalJson.apply(this, args);
-  };
-
-  express.response.send = function sendWithLegacyDeprecationHeaders(...args) {
-    setDeprecationHeaders(this.req, this);
-    return originalSend.apply(this, args);
-  };
-
-  Object.defineProperty(express.response, '__hsBeautyLegacyDeprecationPatchInstalled', {
-    value: true,
-    enumerable: false,
-  });
-}
-
-installLegacyRouteDeprecationPatch();

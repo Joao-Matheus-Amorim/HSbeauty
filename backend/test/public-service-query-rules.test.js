4e3ce2dab94fbc900a346505c8d4b17166ecb9c3
Joao-Matheus-Amorim <202209304731@alunos.estacio.br>
2026-05-25 22:23:19 -0300

    chore: remove public service query test BOM

--- backend/test/public-service-query-rules.test.js
+++ backend/test/public-service-query-rules.test.js
@@ -1,4 +1,4 @@
-﻿import test from 'node:test';
+import test from 'node:test';
 import assert from 'node:assert/strict';
 import { buildPublicServiceQuery } from '../src/public-service-query-rules.js';
 
